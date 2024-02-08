c6f75e1291c471886af16f88d700407e7c261705
Kleuber Silva <kleuber20@hotmail.com>
2024-02-08 11:30:10 -0300

    In development this component

--- src/components/menu/SideMenu.tsx
+++ src/components/menu/SideMenu.tsx
@@ -1,5 +1,12 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+
 export default function SideMenu() {
     return (
-        <section className="h-full w-64 border-r border-r-primary">side menu</section>
+        <section className="h-full w-64 border-r border-r-primary flex items-center justify-center text-center">
+            <div className="flex flex-col gap-3 p-2 text-center justify-center items-center">
+                <ExclamationTriangleIcon className="text-yellow-500 w-32 h-32" />
+                <h1 className="text-xl text-accent-foreground">The menu is under development</h1>
+            </div>
+        </section>
     )
 }
